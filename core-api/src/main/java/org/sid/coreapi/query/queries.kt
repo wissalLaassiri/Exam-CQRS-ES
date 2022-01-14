b48fea2b5c5541658d141957e7f52919d90b8e2a
wissal Laassiri <wissal.laassiri@gmail.com>
2022-01-14 13:37:05 +0100

    second commit

--- core-api/src/main/java/org/sid/coreapi/query/queries.kt
+++ core-api/src/main/java/org/sid/coreapi/query/queries.kt
@@ -1,11 +1,6 @@
 package org.sid.coreapi.query
 
-class GetAllCustomersQuery {}
 class GetAllRadarsQuery {}
 data class GetRadarByIdQuery(
         val id:String
-)
-
-data class GetCustomerByIdQuery(
- val id:String
 )
